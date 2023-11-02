fb6c69c47a16ede71214c8707f66c549312fea49
AnilEnsari <anilensari@gmail.com>
2023-11-02 12:22:55 +0300

    setup done

---
+++ src/main/java/com/example/worktintechspring/WorktintechspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.worktintechspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktintechspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktintechspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worktintechspring/WorktintechspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worktintechspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktintechspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
